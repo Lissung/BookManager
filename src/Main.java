728c68564602df8630afed2f78c11fd7d62d2e88
Jin <estarj815@gmail.com>
2019-03-16 10:36:25 +0900

    BookManager

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    private static System system;
+
+    public static void main(String[] args) {
+        int i=0;
+        int j=5;
+            if(i=0; i<6  ; i++){
+             if(j=5;j<6 ; i--){
+                system.out.print("*");
+            }
+             else break;
+        } else break;
+    }
+}
